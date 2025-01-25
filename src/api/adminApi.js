57ff10a619be6b92464a40f8ab721c45b96a2c1d
vijaya-durga-12 <162143747+vijaya-durga-12@users.noreply.github.com>
2025-01-25 03:57:42 -0800

    Revert "add the admin login data"

--- src/api/adminApi.js
+++
@@ -1,7 +0,0 @@
-// src/api/productApi.js
-import axiosInstance from './axiosInstance';
-
-export const fetchProducts = async () => {
-  const response = await axiosInstance.get('/products/allproducts');
-  return response.data;
-};
